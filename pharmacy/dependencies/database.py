95eda872d166b9337f1707006c0fd798b7d8fb97
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-03 15:07:19 +0000

    Update dependencies.database

--- pharmacy/dependencies/database.py
+++ pharmacy/dependencies/database.py
@@ -2,7 +2,8 @@ from typing import Annotated
 from fastapi import status, Depends
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
-from pharmacy.database.core import sessionmaker
+
+from pharmacy.database.core import SessionMaker
 from pharmacy.database.models.cart_items import CartItem
 from pharmacy.database.models.users import User
 from pharmacy.database.models.inventories import Inventory
@@ -11,7 +12,7 @@ from pharmacy.database.models.admins import Admin
 
 
 def database_connection():
-    with sessionmaker() as connection:
+    with SessionMaker() as connection:
         yield connection
 
 Database = Annotated[Session, Depends(database_connection)]
@@ -23,19 +24,24 @@ def get_user_or_404(db: Database, user_id: int) -> User:
         detail="User not found")
     return user
 
-def get_inventory_or_404(db: Database, inventory_id: int) -> User:
+
+def get_admin_or_404(db: Database, admin_id: int) -> Admin:
+    admin: Admin | None = db.get(Admin, admin_id)
+    if admin is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Admin not found")
+    return admin
+
+
+
+def get_inventory_or_404(db: Database, inventory_id: int) -> Inventory:
     inventory: Inventory | None = db.get(Inventory, inventory_id)
     if inventory is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
         detail="Inventory not found")
     return inventory
 
-def get_admin_or_404(db: Database, adnin_id: int) -> User:
-    admin: Admin | None = db.get(Admin, adnin_id)
-    if admin is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-        detail="Admin not found")
-    return admin
+
 
 def get_cart_item_or_404(db: Database, cart_item_id: int) -> CartItem:
     cart_item: CartItem | None = db.get(CartItem, cart_item_id)
@@ -46,6 +52,6 @@ def get_cart_item_or_404(db: Database, cart_item_id: int) -> CartItem:
     return cart_item
 
 AnnotatedUser = Annotated[User, Depends(get_user_or_404)]
-AnnotatedInventory = Annotated[Inventory, Depends(get_inventory_or_404)]
 AnnotatedAdmin = Annotated[Admin, Depends(get_admin_or_404)]
+AnnotatedInventory = Annotated[Inventory, Depends(get_inventory_or_404)]
 AnnotatedCartItem = Annotated[CartItem, Depends(get_cart_item_or_404)]
