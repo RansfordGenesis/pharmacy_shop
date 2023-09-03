bb0d074d97ccc54cab43b03db266bd63dcbb7ad9
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-03 16:14:42 +0000

    Update models.checkouts

--- pharmacy/database/models/checkouts.py
+++ pharmacy/database/models/checkouts.py
@@ -6,7 +6,7 @@ from pharmacy.database.core import Base
 class Checkout(Base):
     __tablename__ = "checkouts"
 
-    cart_item_id: Mapped[int] = mapped_column(ForeignKey("cart_item.id"), 
+    cart_item_id: Mapped[int] = mapped_column(ForeignKey("cart_items.id"), 
         primary_key=True)
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), primary_key=True)
     subtotal: Mapped[float] = mapped_column(nullable=False)
