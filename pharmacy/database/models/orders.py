8018cc15a5bd1c195d3a7dc341e7592f68ce3a59
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-03 14:55:06 +0000

    Update orders

--- pharmacy/database/models/orders.py
+++ pharmacy/database/models/orders.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from pharmacy.database.core import Base
@@ -7,4 +7,5 @@ class Order(Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     status: Mapped[str] = mapped_column(String, nullable=False)
