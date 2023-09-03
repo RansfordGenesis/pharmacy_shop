dfe0b1d3dc65a8e038b03903cd8d282233f1dd3e
RansfordGenesis <jefferykyeigenesis@gmail.com>
2023-09-03 15:22:42 +0000

    Update routers.inventories

--- pharmacy/routers/inventories.py
+++ pharmacy/routers/inventories.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
-import sqlalchemy
+import sqlalchemy.exc
 from sqlalchemy import select
 
 from pharmacy.schemas.inventories import InventoryCreate, InventorySchema
@@ -11,7 +11,7 @@ from pharmacy.dependencies.database import Database, AnnotatedInventory
 router = APIRouter()
 
 @router.post("/inventories", response_model=InventorySchema)
-def create_inventories(
+def create_inventory(
     inventory_data: InventoryCreate,
     db: Database
 ):
@@ -28,15 +28,18 @@ def create_inventories(
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
         detail="Inventory already exists")
     
+
 @router.get("/inventories", response_model=list[InventorySchema])
 def get_list_of_inventories(db: Database):
     return db.scalar(select(Inventory)).all()
+
 
 @router.get("/inventories/{inventory_id}", response_model=InventorySchema)
 def get_inventory(
     inventory: AnnotatedInventory
 ):
     return inventory
+
 
 @router.delete("/inventories/{inventory_id}")
 def delete_inventory(
